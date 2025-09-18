6562c42c2267fd0439fb6c75a7b103b976340fbb
LihuelZerega <zeregalihuel@gmail.com>
2025-09-18 17:52:09 -0300

    mp fix5

--- medusajs-backend/medusa-config.js
+++ medusajs-backend/medusa-config.js
@@ -72,7 +72,7 @@ const plugins = [
     },
   },
   {
-    resolve: `./src/services/mercadopago-with-shipping`,
+    resolve: `@minskylab/medusa-payment-mercadopago`,
     options: {
       access_token: process.env.MERCADOPAGO_ACCESS_TOKEN,
       success_backurl: process.env.MERCADOPAGO_SUCCESS_BACKURL,
